740f913bc93db56761c854184baad57488869d87
DowonKim <kimdowon1404@gmail.com>
2023-09-21 23:58:46 +0900

    feat :주석 추가

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -18,17 +18,21 @@ interface FormData {
  * 1. 비제어 컴포넌트 방식으로 구현되어있기에 렌더링 이슈를 해결
  * 2. props drilling 없이 사용할 수 있다
  * 3. 데이터는 계속 동기화되며 분산되지 않아 데이터를 한 곳에서 관리 가능
+ * 
+ * 학습 내용
+✅ register (form의 유효성 확인)
+✅ handleSubmit (form의 제출)
+✅ formState (form의 전체 상태양식 확인 ex. error)
+✅ watch (form의 실시간 입력값 확인)
+✅ defaultValues (form에서 정한 value의 가장 초기값)
+✅ setError (form에서 errors이외의 custom error생성을 위해)
+✅ required (form에 추가된 입력값의 요구사항 메시지)
  *
  * @author kimdowon
  * @Date 23.09.21.
  * @returns react hook form을 이용한 편리한 유효성 검사
  */
 const Auth = () => {
-  // State
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [userName, setUserName] = useState("");
   // 유효성 검사
   const {
     register,
